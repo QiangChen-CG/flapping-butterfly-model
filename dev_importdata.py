ef78e0516afad2fa24277485187af4b19e4432fb
TylerWilson0407 <tyler.wilson.0407@gmail.com>
2017-10-25 01:02:35 -0700

    Started work on dev_wingforces.py, working on code to rotate wing points by the specified wing angles.

---
+++ dev_importdata.py
@@ -0,0 +1,56 @@
+"""Converts data from MATLAB .mat files into usable python data.
+
+This function extracts data in MATLAB's .mat file format and converts it
+into usable python data.
+
+This module will run as a script and will not be called or referenced
+elsewhere in this project
+
+Imports and collects data into a ButterflyData object, to be saved
+
+"""
+import numpy as np
+import scipy.io
+import buildbutterfly
+import pickle
+
+
+def convert_mat_matrix(filename, matname):
+    """Convert a MATLAB matrix(matname) contained in a .mat file(filename)
+    into a numpy.ndarray
+
+    filename = name of .mat file with path, e.g. 'matrixfiles/mymatrix.mat'
+    matname = name of matrix saved in .mat file, e.g. 'myMatrix'
+
+    """
+    mat_file = scipy.io.loadmat(filename)
+    matrix = mat_file[matname]
+    return matrix
+
+
+wing_array = convert_mat_matrix('MATLAB_files/TreeNymphLeftWing.mat', 'Wing')
+wing_array = wing_array.transpose()
+wing_array = wing_array / 1000  # Convert units from mm to meter
+wing_coords = {
+    'x': wing_array[0],
+    'y_le': wing_array[1],
+    'y_te': wing_array[2]
+}
+# treeNymphHead = buildbutterfly.BodySphere(0.02, 0.006)
+# treeNymphThorax = buildbutterfly.BodyEllipse(0.1808, 0.01, 0.006, 10)
+# treeNymphAbdomen = buildbutterfly.BodyEllipse(0.2404, 0.0289, 0.006, 10)
+# treeNymphWing = buildbutterfly.Wing(wing_coords, 0.0587, 25)
+# treeNymph = buildbutterfly.Butterfly(treeNymphHead, treeNymphThorax,
+#                                      treeNymphAbdomen, treeNymphWing)
+#
+#
+# output = open('treeNymph.pkl', 'wb')
+# pickle.dump(treeNymph, output, pickle.HIGHEST_PROTOCOL)
+
+print(wing_array[0])
+
+wing_array = np.around(wing_array, 10)
+myList = ', '.join(map(str, wing_array[2]))
+
+
+print('[' + myList + ']')
